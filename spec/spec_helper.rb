844237a2290d5cfe41bea7be139d996e745b676f
meghal <meghalbulsara22@gmail.com>
2017-08-18 14:39:09 +0530

    Fix spec_helper file error

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,26 +16,20 @@
 
 
 RSpec.configure do |config|
-  config.after(:example){
-    puts "I just ran a task"
-  }
 
-  config.before(:suite){
-    puts "Welcome!"
-  }
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
-  # config.expect_with :rspec do |expectations|
-  # This option will default to `true` in RSpec 4. It makes the `description`
-  # and `failure_message` of custom matchers include text for helper methods
-  # defined using `chain`, e.g.:
-  #     be_bigger_than(2).and_smaller_than(4).description
-  #     # => "be bigger than 2 and smaller than 4"
-  # ...rather than:
-  #     # => "be bigger than 2"
-  # expectations.include_chain_clauses_in_custom_matcher_descriptions = true
-  # end
+  config.expect_with :rspec do |expectations|
+    # This option will default to `true` in RSpec 4. It makes the `description`
+    # and `failure_message` of custom matchers include text for helper methods
+    # defined using `chain`, e.g.:
+    #     be_bigger_than(2).and_smaller_than(4).description
+    #     # => "be bigger than 2 and smaller than 4"
+    # ...rather than:
+    #     # => "be bigger than 2"
+    # expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
 
   # rspec-mocks config goes here. You can use an alternate test double
   # library (such as bogus or mocha) by changing the `mock_with` option here.
